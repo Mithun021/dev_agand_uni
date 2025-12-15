a9f785c36c4dbdcc9a4987b4b65ef730b8beeba3
Mithun Kumar <mithunkr79038@gmail.com>
2025-12-15 13:15:32 +0530

    master dashboard

--- resources/views/backend/courses/create.blade.php
+++ resources/views/backend/courses/create.blade.php
@@ -13,31 +13,31 @@
                 @csrf
             <div class="card-body">
                 <div class="row">
-                    <div class="col-md-12">
+                    <div class="col-md-12 form-group">
                         <label for="">Course Name<span class="text-danger">*</span></label>
                         <input type="text" class="form-control" name="name">
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 form-group">
                         <label for="">Course Code</label>
                         <input type="text" class="form-control" name="course_type">
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 form-group">
                         <label for="">Course Type<span class="text-danger">*</span></label>
                         <select name="course_type" class="form-control">
                             <option value="Annual Based">Annual Based</option>
                             <option value="Semester Based">Semester Based</option>
                         </select>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 form-group">
                         <label for="">Semester</label>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 form-group">
                         <label for="">Scheme<span class="text-danger">*</span></label>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 form-group">
                         <label for="">Session<span class="text-danger">*</span></label>
                     </div>
-                    <div class="col-md-12">
+                    <div class="col-md-12 form-group">
                         <label for="">Institute<span class="text-danger">*</span></label>
                     </div>
                 </div>
